860e6b5ce872bfb2b75a355c902c6c39f5197899
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-04-26 16:05:49 +1000

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -8,3 +8,17 @@ function math_screen() {
 
     window.location = "game.html";
 }
+function send() {
+    number1 =  document.getElementById("number1").value;
+    number2 =  document.getElementById("number2").value;
+    answer = parseInt(number1) * parseInt(number2);
+
+    question_numbers = "<h4>" +  number1 + " X " + number2 + "</h4>";
+    input_box = "<br>Answer :<input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn_info' onclick='check()'>Check</button>";
+    row = question_numbers + input_box + check_button ;
+
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
